19079fb7b677f1227e92fb4c0d67a7256cd23341
Connor Clark <cjamcl@google.com>
2022-05-25 17:03:38 -0700

    trying to get topbar test working...

--- flow-report/test/topbar-test.tsx
+++ flow-report/test/topbar-test.tsx
@@ -34,54 +34,58 @@ const flowResult = {
 let wrapper: FunctionComponent;
 let options: LH.FlowReportOptions;
 
-beforeEach(() => {
-  mockSaveFile.mockReset();
-  options = {};
-  wrapper = ({children}) => (
-    <OptionsContext.Provider value={options}>
-      <FlowResultContext.Provider value={flowResult}>
-        <I18nProvider>
-          {children}
-        </I18nProvider>
-      </FlowResultContext.Provider>
-    </OptionsContext.Provider>
-  );
+describe('Topbar', () => {
+  beforeEach(() => {
+    mockSaveFile.mockReset();
+    options = {};
+    wrapper = ({children}) => (
+      <OptionsContext.Provider value={options}>
+        <FlowResultContext.Provider value={flowResult}>
+          <I18nProvider>
+            {children}
+          </I18nProvider>
+        </FlowResultContext.Provider>
+      </OptionsContext.Provider>
+    );
+  });
+
+  // TODO(mocha): mock not being called
+  it('save button opens save dialog for HTML file', async () => {
+    options = {getReportHtml: () => '<html></html>'};
+    const root = render(<Topbar onMenuClick={() => {}}/>, {wrapper});
+
+    const saveButton = root.getByText('Save');
+    // saveButton.click();
+    await act(() => saveButton.click());
+
+    expect(mockSaveFile).toHaveBeenCalledWith(
+      expect.any(Blob),
+      'User-flow_2021-09-14_22-24-22.html'
+    );
+  });
+
+  it('provides save as gist option if defined', async () => {
+    const saveAsGist = jestMock.fn();
+    options = {saveAsGist};
+    const root = render(<Topbar onMenuClick={() => {}}/>, {wrapper});
+
+    const saveButton = root.getByText('Save as Gist');
+    saveButton.click();
+
+    expect(saveAsGist).toHaveBeenCalledWith(flowResult);
+  });
+
+  it('toggles help dialog', async () => {
+    const root = render(<Topbar onMenuClick={() => {}}/>, {wrapper});
+
+    expect(root.queryByText(/Use Navigation reports to/)).toBeFalsy();
+    const helpButton = root.getByText('Understanding Flows');
+
+    await act(() => helpButton.click());
+    expect(root.getByText(/Use Navigation reports to/)).toBeTruthy();
+
+    await act(() => helpButton.click());
+    expect(root.queryByText(/Use Navigation reports to/)).toBeFalsy();
+  });
 });
 
-// TODO(mocha): mock not being called
-it.skip('save button opens save dialog for HTML file', async () => {
-  options = {getReportHtml: () => '<html></html>'};
-  const root = render(<Topbar onMenuClick={() => {}}/>, {wrapper});
-
-  const saveButton = root.getByText('Save');
-  saveButton.click();
-
-  expect(mockSaveFile).toHaveBeenCalledWith(
-    expect.any(Blob),
-    'User-flow_2021-09-14_22-24-22.html'
-  );
-});
-
-it('provides save as gist option if defined', async () => {
-  const saveAsGist = jestMock.fn();
-  options = {saveAsGist};
-  const root = render(<Topbar onMenuClick={() => {}}/>, {wrapper});
-
-  const saveButton = root.getByText('Save as Gist');
-  saveButton.click();
-
-  expect(saveAsGist).toHaveBeenCalledWith(flowResult);
-});
-
-it('toggles help dialog', async () => {
-  const root = render(<Topbar onMenuClick={() => {}}/>, {wrapper});
-
-  expect(root.queryByText(/Use Navigation reports to/)).toBeFalsy();
-  const helpButton = root.getByText('Understanding Flows');
-
-  await act(() => helpButton.click());
-  expect(root.getByText(/Use Navigation reports to/)).toBeTruthy();
-
-  await act(() => helpButton.click());
-  expect(root.queryByText(/Use Navigation reports to/)).toBeFalsy();
-});
